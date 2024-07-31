eb0f796cf6afd59557bc8530a2c8bed20bd21d84
AdeedKhan <Muhammadadeedk@gmail.com>
2024-07-31 22:24:29 +0500

    Encapsulation and also using C# properties

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Encapsulation
+{
+    //class Account
+    //{
+    //    int accountBalance = 1000;
+
+    //    public void SetBalance(int amount)
+    //    {
+    //        if (amount<0)
+    //        {
+    //            Console.WriteLine("You can't pass negative value");
+    //        }
+    //        else
+    //        {
+    //            accountBalance = amount;
+    //        }
+    //    }
+    //    public void GetBalance() 
+    //    {
+    //        Console.WriteLine("Your Account Balance is:"+accountBalance);
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //Console.WriteLine("Hello, World!");
+    //        Account myAccount = new Account();
+    //        myAccount.SetBalance(-10000);
+    //        myAccount.GetBalance();
+    //        Console.ReadLine();
+    //    }
+    //}
+
+    //C# Properties (Same above Code through C# Properties)
+
+    class Account
+    {
+        int accountBalance = 1000;
+        public int Balance
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("YOu can't pass negative value");
+                }
+                else
+                {
+                    accountBalance = value;
+                }
+            }
+            get 
+            { 
+                return accountBalance;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+            myAccount.Balance = -10000;
+            Console.WriteLine("Your account balance is:" +myAccount.Balance);
+            Console.ReadLine();
+        }
+    }
+
+
+}
